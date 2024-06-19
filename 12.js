ec379671c315d105d7f63a2700ff761aa1c096fc
Darya Matskevich <darramatskevich@gmail.com>
2024-06-19 14:14:58 +0300

    opposite

---
+++ 12.js
@@ -0,0 +1,8 @@
+// Very simple, given a number(integer / decimal / both depending on the language),
+// find its opposite(additive inverse).
+
+// Solution
+
+function opposite(number) {
+    return -number;
+}
