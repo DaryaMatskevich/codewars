4f7a4e0a5304fc9cf79049dde5632804ab228851
Darya Matskevich <darramatskevich@gmail.com>
2024-06-23 22:43:12 +0300

    solution

---
+++ 23.js
@@ -0,0 +1,10 @@
+// Instructions
+// Complete the solution so that it reverses the string passed into it.
+
+// Solution
+
+function solution(str) {
+    let array = str.split("");
+    let newArray = array.reverse();
+    return newArray.join("")
+}
